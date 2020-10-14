94e57b12c4717093e4f940d9a2e1bfcd1f4d2a56
Will O'Beirne <will@coder.com>
2020-10-14 16:36:59 -0500

    Fix build-push-action name

--- bin/generate-actions-yaml.sh
+++ bin/generate-actions-yaml.sh
@@ -15,7 +15,7 @@ function appendAction() {
   {
     echo "      -"
     echo "        name: Build and push - $image:$tag"
-    echo "        uses: docker/build-and-push@v2"
+    echo "        uses: docker/build-push-action@v2"
     echo "        with:"
     echo "          file: ./images/$image/Dockerfile.$tag"
     echo "          tags: \${{ secrets.DOCKERHUB_USERNAME }}/enterprise-$image:$tag"
