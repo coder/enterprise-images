89460d3c562a6260a1c713ad53939687dbf19b06
Victoria <Bernard.victoria22@gmail.com>
2020-10-14 11:44:04 -0500

    Teffens original startup script

---
+++ tigervnc/startupvnc.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+### every exit != 0 fails the script
+set -e
+
+## print out help
+help (){
+echo "
+USAGE:
+docker run -it -p 6901:6901 -p 5901:5901 consol/<image>:<tag> <option>
+
+IMAGES:
+consol/ubuntu-xfce-vnc
+consol/centos-xfce-vnc
+consol/ubuntu-icewm-vnc
+consol/centos-icewm-vnc 
+
+TAGS:
+latest  stable version of branch 'master'
+dev     current development version of branch 'dev'
+
+OPTIONS:
+-w, --wait      (default) keeps the UI and the vncserver up until SIGINT or SIGTERM will received
+-s, --skip      skip the vnc startup and just execute the assigned command.
+                example: docker run consol/centos-xfce-vnc --skip bash
+-d, --debug     enables more detailed startup output
+                e.g. 'docker run consol/centos-xfce-vnc --debug bash'
+-h, --help      print out this help
+
+Fore more information see: https://github.com/ConSol/docker-headless-vnc-container
+"
+}
+if [[ $1 =~ -h|--help ]]; then
+    help
+    exit 0
+fi
+
+# add `--skip` to startup args, to skip the VNC startup procedure
+if [[ $1 =~ -s|--skip ]]; then
+    echo -e "\n\n------------------ SKIP VNC STARTUP -----------------"
+    echo -e "\n\n------------------ EXECUTE COMMAND ------------------"
+    echo "Executing command: '${@:2}'"
+    exec "${@:2}"
+fi
+if [[ $1 =~ -d|--debug ]]; then
+    echo -e "\n\n------------------ DEBUG VNC STARTUP -----------------"
+    export DEBUG=true
+fi
+
+## correct forwarding of shutdown signal
+cleanup () {
+    kill -s SIGTERM $!
+    exit 0
+}
+trap cleanup SIGINT SIGTERM
+
+## write correct window size to chrome properties
+# $STARTUPDIR/chrome-init.sh
+
+## resolve_vnc_connection
+VNC_IP=$(hostname -i)
+
+## change vnc password
+echo -e "\n------------------ change VNC password  ------------------"
+# first entry is control, second is view (if only one is valid for both)
+mkdir -p "$HOME/.vnc"
+PASSWD_PATH="$HOME/.vnc/passwd"
+
+if [[ -f $PASSWD_PATH ]]; then
+    echo -e "\n---------  purging existing VNC password settings  ---------"
+    rm -f $PASSWD_PATH
+fi
+
+if [[ $VNC_VIEW_ONLY == "true" ]]; then
+    echo "start VNC server in VIEW ONLY mode!"
+    #create random pw to prevent access
+    echo $(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20) | vncpasswd -f > $PASSWD_PATH
+fi
+echo "$VNC_PW" | vncpasswd -f >> $PASSWD_PATH
+chmod 600 $PASSWD_PATH
+
+
+## start vncserver and noVNC webclient
+echo -e "\n------------------ start noVNC  ----------------------------"
+if [[ $DEBUG == true ]]; then echo "$NO_VNC_HOME/utils/launch.sh --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT"; fi
+$NO_VNC_HOME/utils/launch.sh --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT &> $STARTUPDIR/no_vnc_startup.log &
+PID_SUB=$!
+
+echo -e "\n------------------ start VNC server ------------------------"
+echo "remove old vnc locks to be a reattachable container"
+vncserver -kill $VNC_DISPLAY_ID &> $STARTUPDIR/vnc_startup.log \
+    || rm -rfv /tmp/.X*-lock /tmp/.X11-unix &> $STARTUPDIR/vnc_startup.log \
+    || echo "no locks present"
+
+echo -e "start vncserver with param: VNC_COL_DEPTH=$VNC_COL_DEPTH, VNC_RESOLUTION=$VNC_RESOLUTION\n..."
+if [[ $DEBUG == true ]]; then echo "vncserver $VNC_DISPLAY_ID -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION"; fi
+vncserver -cleanstale -SecurityTypes None,TLSNone $VNC_DISPLAY_ID -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION -xstartup $VNC_XSTARTUP &> $STARTUPDIR/no_vnc_startup.log
+# 
+
+## log connect options
+echo -e "\n\n------------------ VNC environment started ------------------"
+echo -e "\nVNCSERVER started on DISPLAY= $VNC_DISPLAY_ID \n\t=> connect via VNC viewer with $VNC_IP:$VNC_PORT"
+echo -e "\nnoVNC HTML client started:\n\t=> connect via http://$VNC_IP:$NO_VNC_PORT/?password=...\n"
+
+
+if [[ $DEBUG == true ]] || [[ $1 =~ -t|--tail-log ]]; then
+    echo -e "\n------------------ $HOME/.vnc/*$VNC_DISPLAY_ID.log ------------------"
+    # if option `-t` or `--tail-log` block the execution and tail the VNC log
+    tail -f $STARTUPDIR/*.log $HOME/.vnc/*$VNC_DISPLAY_ID.log
+fi
+
+if [ -z "$1" ] || [[ $1 =~ -w|--wait ]]; then
+    wait $PID_SUB
+else
+    # unknown option ==> call command
+    echo -e "\n\n------------------ EXECUTE COMMAND ------------------"
+    echo "Executing command: '$@'"
+    exec "$@"
