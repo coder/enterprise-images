f8bd8ada5e99ced4c33d608e3b8efa5f52f3b1ec
Teffen Ellis <teffen@nirri.us>
2020-10-24 01:21:05 +0000

    Swap tigervnc server for turbovnc server.
    - This appears to fix several render issues in Firefox and bumps performance quite a bit.

--- images/vnc/vnc/scripts/vncserver.sh
+++ images/vnc/vnc/scripts/vncserver.sh
@@ -6,21 +6,15 @@ mkdir -p $VNC_LOG_DIR
 if [[ -v VNC_DISPLAY_ID ]]; then
   echo "Removing previous VNC locks..."
 
-  vncserver -kill "$VNC_DISPLAY_ID" &> "$VNC_LOG_DIR/vnc_startup.log"
-  rm -rfv /tmp/.X90-lock /tmp/.X11-unix &> "$VNC_LOG_DIR/vnc_startup.log"
+  vncserver -kill "$VNC_DISPLAY_ID" &> "$VNC_LOG_DIR/vnc_startup.log" || true
+  rm -rfv /tmp/.X90-lock /tmp/.X11-unix &> "$VNC_LOG_DIR/vnc_startup.log" || true
 fi
 
 echo -e "Starting vncserver with param: VNC_COL_DEPTH=$VNC_COL_DEPTH, VNC_RESOLUTION=$VNC_RESOLUTION\n..."
 
 exec vncserver "$VNC_DISPLAY_ID" \
- -cleanstale \
  -SecurityTypes None \
- -AlwaysShared \
- -AcceptKeyEvents \
- -AcceptPointerEvents \
- -AcceptSetDesktopSize \
- -SendCutText \
- -AcceptCutText \
+ -alwaysshared \
  -depth "$VNC_COL_DEPTH" \
  -geometry "$VNC_RESOLUTION" \
  -fg \
