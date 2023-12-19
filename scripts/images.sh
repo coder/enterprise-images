a5e27b26a20b610e0968ef6a38c3ab8eb3cf8ecf
Cian Johnston <cian@coder.com>
2023-12-19 12:37:41 +0000

    chore: update README, stop building deprecated images

--- scripts/images.sh
+++ scripts/images.sh
@@ -3,26 +3,6 @@
 set -euo pipefail
 
 IMAGES=(
-  # This image must always be built first.
   "base"
-
-  # These must be built first because others build FROM these
-  "configure"
-  "golang"
-  "java"
-  "multieditor"
-  "node"
-  "ruby"
-  "rust"
-  "vnc"
-
-  # We can build these images in any order
-  "android"
-  "clion"
-  "goland"
-  "intellij"
   "minimal"
-  "jupyter"
-  "pycharm"
-  "webstorm"
 )
